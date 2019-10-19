fa018d55c670503ff71884704b08445bbd8451e0
Oleksandr Sovenko <info@oleksandrsovenko.com>
2019-10-19 23:25:34 +0000

    * Added support device HC-SR04

--- helpers/pin-event-change.c
+++ helpers/pin-event-change.c
@@ -1,16 +1,16 @@
 // pin-event-change
 // Copyright (C) 2019 Oleksandr Sovenko (info@oleksandrsovenko.com)
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -21,33 +21,97 @@
 
 #include <wiringPi.h>
 
+/** device_hc_sr04
+ * @param trig
+ * @param echo
+ */
+void device_hc_sr04(int trig, int echo) {
+	pinMode(trig, OUTPUT);
+	pinMode(echo, INPUT);
+
+	//TRIG pin must start LOW
+	digitalWrite(trig, LOW);
+	delay(30);
+}
+
+/** device_hc_sr04_getcm
+ * @param trig
+ * @param echo
+ */
+int device_hc_sr04_getcm(int trig, int echo) {
+	digitalWrite(trig, HIGH);
+	delayMicroseconds(20);
+	digitalWrite(trig, LOW);
+
+	//Wait for echo start
+	while(digitalRead(echo) == LOW);
+
+	//Wait for echo end
+	long startTime = micros();
+	while(digitalRead(echo) == HIGH);
+	long travelTime = micros() - startTime;
+
+	//Get distance in cm
+	int distance = travelTime / 58;
+
+	return distance;
+}
+
+/** main
+ * @param argc
+ * @param argv
+ */
 int main(int argc, char *argv[]) {
-	if (argc < 4)
+	if (argc < 2)
 		return EXIT_FAILURE;
 
-	char *mode    = argv[1];
-	int  pin      = atoi(argv[2]);
-	int  interval = atoi(argv[3]);
+	char *device  = argv[1];
 
 	wiringPiSetup();
 
-	if (!strcmp(mode, "output"))
-		pinMode(pin, OUTPUT);
+	/** HC-SR501
+ 	 */
+	if (!strcmp(device, "hc-sr501")) {
 
-	if (!strcmp(mode, "input"))
-		pinMode(pin, INPUT);
+	}
 
-	int old_status = digitalRead(pin);
-  
-	for (;;) {
-		int new_status = digitalRead(pin);
+ 	/** MPU-6050
+ 	 */
+	if (!strcmp(device, "mpu-6050")) {
 
-		if (new_status != old_status) {
-			fprintf(stderr, "{\"event\":\"change\",\"pin\":\"%i\",\"value\":\"%i\"}\n", pin, new_status);
-			old_status = new_status;
+	}
+
+	/** HC-SR04
+	 * $example: ./pin-event-change hc-sr04 9 8
+ 	 */
+	if (!strcmp(device, "hc-sr04")) {
+		if (argc < 4) {
+			return EXIT_FAILURE;
 		}
 
-		delay(interval);
+		int trig = atoi(argv[2]);
+		int echo = atoi(argv[3]);
+
+		device_hc_sr04(trig, echo);
+
+		int old_distance = device_hc_sr04_getcm(trig, echo);
+
+		for (;;) {
+			int new_distance = device_hc_sr04_getcm(trig, echo);
+
+			if (new_distance != old_distance) {
+				fprintf(stderr, "{\"device\":\"hc-sr04\",\"pin\":{\"trig\":%d,\"echo\":%d},\"distance-cm\":%d}\n", trig, echo, new_distance);
+				old_distance = new_distance;
+			}
+
+			delay(1000);
+		}
+	}
+
+ 	/** DS18B20
+ 	 */
+	if (!strcmp(device, "ds18b20")) {
+
 	}
 
 	return EXIT_SUCCESS;
