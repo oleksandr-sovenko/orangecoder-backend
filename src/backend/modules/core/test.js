a82dc677926fd1603a48d8139b66d17e8e0a5d84
Oleksandr Sovenko <info@oleksandrsovenko.com>
2020-02-29 11:22:03 +0000

    Changes

---
+++ src/backend/modules/core/test.js
@@ -0,0 +1,19 @@
+var { BMP280, GPIO, HC_SC04 } = require('./build/Release/BMP280');
+
+BMP280.init();
+
+var gpio14  = GPIO.output(14);
+var hc_sc04 = HC_SC04(15, 16);
+
+setInterval(function() {
+	data = BMP280.data();
+
+	var sm = hc_sc04.data();
+
+	console.log(data, hc_sc04.data(), gpio14.isLow(), gpio14.isHigh());
+
+	if (sm < 20)
+		gpio14.high();
+	else
+		gpio14.low();
+}, 1000);
