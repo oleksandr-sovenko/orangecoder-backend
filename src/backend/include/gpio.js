5b122e9dcff07035d27e0d3e7d1b97e1715bc340
Oleksandr Sovenko <info@oleksandrsovenko.com>
2020-08-30 20:12:28 +0300

    Back-end: Added /gpio

--- src/backend/include/gpio.js
+++ src/backend/include/gpio.js
@@ -16,12 +16,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-const fs = require('fs'),
-      { spawn } = require('child_process'),
-      gpio_json = global.config.dir.data + '/gpio.json';
-
-
-global.childs = [];
+const { exec } = require('child_process');
 
 
 async function routes(fastify, options) {
@@ -31,8 +26,8 @@ async function routes(fastify, options) {
     fastify.get('/gpio', async function(request, reply) {
         const { stdout, stderr } = await exec('gpio readall');
 
-        var temp = stdout.split('\n'),
-            data = [],
+        var temp   = stdout.split('\n'),
+            data   = [],
             fields = [];
 
         for (var i in temp) {
@@ -61,165 +56,7 @@ async function routes(fastify, options) {
 
         return { success: true, msg:'', data: data }
     })
-
-
-    /** Get list of devices
-     *  @endpoint /gpio/devices
-     *  @method   GET
-     */
-    fastify.get('/gpio/devices', async function(req, rep) {
-        if (!is_authorized(req))
-            return { success: false, msg: 'Authorization required' }
-
-        if (fs.existsSync(gpio_json))
-            var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8'));
-        else
-            var devices = [];
-
-        return { success: true, msg:'', data: devices }
-    })
-
-
-    /** Add new device
-     *  @endpoint /gpio/device
-     *  @method   PUT
-     */
-    fastify.put('/gpio/device', async function(req, rep) {
-        if (!is_authorized(req))
-            return { success: false, msg: 'Authorization required' }
-
-        if (req.body.name === undefined || req.body.device === undefined || req.body.pins === undefined)
-            return { success: false, msg: 'Require name, device and pins' };
-
-        if (req.body.name === '' || req.body.device === '' || Object.keys(req.body.pins).length === 0)
-            return { success: false, msg: "Fields name, device, pins can't be empty." };
-
-        if (fs.existsSync(gpio_json))
-            var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8'));
-        else
-            var devices = [];
-
-        devices.push({ id: uuid4(), name: req.body.name,  device: req.body.device,  pins: req.body.pins })
-        fs.writeFileSync(gpio_json, JSON.stringify(devices));
-        spawn_helpers();
-
-
-        return { success: true, msg:'' }
-    })
-
-
-    /** Get data of the device
-     *  @endpoint /gpio/device/:id
-     *  @method   GET
-     */
-    fastify.get('/gpio/device/:id', async function(req, rep) {
-        if (!is_authorized(req))
-            return { success: false, msg: 'Authorization required' }
-
-        if (fs.existsSync(gpio_json)) {
-            var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8')),
-                filter = [];
-
-            for (var i in devices) {
-                if (devices[i].id === req.params.id)
-                    return devices[i];
-            }
-        }
-
-        return {}
-    })
-
-
-    /** Update a device
-     *  @endpoint /gpio/device/:id
-     *  @method   POST
-     */
-    fastify.post('/gpio/device/:id', async function(req, rep) {
-        if (!is_authorized(req))
-            return { success: false, msg: 'Authorization required' }
-
-        if (req.body.name === undefined || req.body.device === undefined || req.body.pins === undefined)
-            return { success: false, msg: 'Require name, device and pins' };
-
-        if (req.body.name === '' || req.body.device === '' || Object.keys(req.body.pins).length === 0)
-            return { success: false, msg: "Fields name, device, pins can't be empty." };
-
-        if (fs.existsSync(gpio_json)) {
-            var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8')),
-                filter = [];
-
-            for (var i in devices) {
-                if (devices[i].id === req.params.id)
-                    devices[i] = {
-                        id    : devices[i].id,
-                        name  : req.body.name,
-                        device: req.body.device,
-                        pins  : req.body.pins
-                    };
-            }
-
-            fs.writeFileSync(gpio_json, JSON.stringify(devices));
-        }
-
-        spawn_helpers();
-
-        return { success: true, msg:'' }
-    })
-
-
-    /** Remove a device
-     *  @endpoint /gpio/device/:id
-     *  @method   DELETE
-     */
-    fastify.delete('/gpio/device/:id', async function(req, rep) {
-        if (!is_authorized(req))
-            return { success: false, msg: 'Authorization required' }
-
-        if (fs.existsSync(gpio_json)) {
-            var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8')),
-                filter = [];
-
-            for (var i in devices) {
-                if (devices[i].id !== req.params.id)
-                    filter.push(devices[i]);
-            }
-
-            fs.writeFileSync(gpio_json, JSON.stringify(filter));
-        }
-
-        spawn_helpers();
-
-        return { success: true, msg:'' }
-    })
 }
-
-
-/**
- *
- */
-function spawn_helpers() {
-    // spawn('killall', ['pin-event-change']);
-
-    // global.childs = [];
-
-    // if (fs.existsSync(gpio_json))
-    //     var devices = JSON.parse(fs.readFileSync(gpio_json, 'utf8'));
-    // else
-    //     var devices = [];
-
-    // for (var i in devices) {
-    //     var pins =  JSON.stringify(devices[i].pins)
-    //                 .replace(/{/g, '')
-    //                 .replace(/"/g, '')
-    //                 .replace(/,/g, ' ')
-    //                 .replace(/}/g, '');
-
-    //     global.helperPinEventChange([devices[i].device, devices[i].id, pins]);
-    // }
-}
-
-
-spawn_helpers();
 
 
 module.exports = routes
